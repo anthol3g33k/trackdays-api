a29697c21d80902051a68b1aec7a9d2559566f78
anthol3g33k <lannoy.anthony@gmail.com>
2018-09-06 22:12:55 +0200

    Authentication module

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
+var cors = require('cors')
 var indexRouter = require('./routes/AppController')
 var usersRouter = require('./routes/UserController')
 var partnersRouter = require('./routes/PartnerController')
 var eventsRouter = require('./routes/EventController')
-var config = require('config')
-var mongoose = require('mongoose')
-var cors = require('cors')
+var authRouter = require('./routes/AuthController')
+var db = require('./db');
 
 var app = express()
 
@@ -24,20 +24,13 @@ app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/partners', partnersRouter)
 app.use('/events', eventsRouter)
+app.use('/api/auth', authRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404))
 })
 
-//Set up default mongoose connection
-var mongoDB = config.mongodb.uri
-mongoose.connect(mongoDB, { useNewUrlParser: true })
-// Get Mongoose to use the global promise library
-mongoose.Promise = global.Promise
-//Get the default connection
-var db = mongoose.connection
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 
 module.exports = app
--- routes/EventController.js
+++ routes/EventController.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
 var config = require('config')
 var Event = require('../models/EventModel')
+var VerifyToken = require('./VerifyToken')
 
 /* GET events list. */
 router.get('/', function(req, res, next) {
